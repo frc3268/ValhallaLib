622bcb2cdaa0dcbd96c68a5ee2a579676d77c2c6
fernir3268 <116040382+fernir3268@users.noreply.github.com>
2026-02-11 16:33:24 -0800

    more timer stuff

--- src/main/kotlin/frc/robot/TimerTelemetry.kt
+++ src/main/kotlin/frc/robot/TimerTelemetry.kt
@@ -1,6 +1,7 @@
 package frc.robot
 
 import edu.wpi.first.units.Units
+import edu.wpi.first.wpilibj.Timer
 import edu.wpi.first.wpilibj2.command.SubsystemBase
 import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard
@@ -8,9 +9,14 @@ import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard
 class TimerTelemetry : SubsystemBase() {
 
     private val timerTab = Shuffleboard.getTab(Constants.GENERAL_TAB)
-    private var Timer = timerTab.add("Time:", 0.0).entry
+    private var TimerWidget = timerTab.add("Time:", 0.0).entry
+    private val timer = Timer()
+
+    init{
+        timer.Start()
+    }
 
     override fun periodic() {
-        // Timer.setDouble()
+        TimerWidget.setDouble(timer.get())
     }
 }
